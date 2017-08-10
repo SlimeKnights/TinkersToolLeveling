1d64ebd5a100c4db962c6db88aad5c396cd79412
DerBaM <derbam@github.com>
2017-08-10 18:38:51 +0200

    fix levelup ding/sound not played during server play

--- src/main/java/slimeknights/toolleveling/ClientProxy.java
+++ src/main/java/slimeknights/toolleveling/ClientProxy.java
@@ -3,32 +3,15 @@ package slimeknights.toolleveling;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.SoundEvent;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextFormatting;
-import slimeknights.tconstruct.common.Sounds;
 
 public class ClientProxy extends CommonProxy {
-  private static final SoundEvent SOUND_LEVELUP = registerSound();
-
-  @Override
-  public void playLevelupDing(EntityPlayer player) {
-    Sounds.PlaySoundForPlayer(player, SOUND_LEVELUP, 1f, 1f);
-  }
-
   @Override
   public void sendLevelUpMessage(int level, ItemStack itemStack, EntityPlayer player) {
     player.sendMessage(new TextComponentString(
         TextFormatting.DARK_AQUA + I18n.format("message.levelup",
         itemStack.getDisplayName() + TextFormatting.DARK_AQUA, level)
     ));
-  }
-
-  private static SoundEvent registerSound() {
-    ResourceLocation location = new ResourceLocation(TinkerToolLeveling.MODID, "levelup");
-    SoundEvent event = new SoundEvent(location);
-    event.setRegistryName(location);
-    return event;
   }
 }
--- src/main/java/slimeknights/toolleveling/CommonProxy.java
+++ src/main/java/slimeknights/toolleveling/CommonProxy.java
@@ -2,11 +2,18 @@ package slimeknights.toolleveling;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvent;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.util.text.TextFormatting;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import slimeknights.tconstruct.common.Sounds;
 
 public class CommonProxy {
+  private static final SoundEvent SOUND_LEVELUP = registerSound();
+
   public void playLevelupDing(EntityPlayer player) {
+    Sounds.PlaySoundForPlayer(player, SOUND_LEVELUP, 1f, 1f);
   }
 
   public void sendLevelUpMessage(int level, ItemStack itemStack, EntityPlayer player) {
@@ -19,5 +26,13 @@ public class CommonProxy {
     componentText.getStyle().setColor(TextFormatting.DARK_AQUA);
 
     player.sendMessage(componentText);
+  }
+
+  private static SoundEvent registerSound() {
+    ResourceLocation location = new ResourceLocation(TinkerToolLeveling.MODID, "levelup");
+    SoundEvent event = new SoundEvent(location);
+    event.setRegistryName(location);
+    GameRegistry.findRegistry(SoundEvent.class).register(event);
+    return event;
   }
 }
