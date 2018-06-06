3f243d6296a927452ff675a50cb73fe541285a08
Bernhard Bonigl <bernhard.bonigl@gmail.com>
2018-06-06 19:32:35 +0200

    Update versions and bump to 1.0.5

--- src/main/java/slimeknights/toolleveling/TinkerToolLeveling.java
+++ src/main/java/slimeknights/toolleveling/TinkerToolLeveling.java
@@ -6,12 +6,10 @@ import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.registries.IForgeRegistry;
 
 import java.io.File;
 
@@ -27,7 +25,7 @@ import slimeknights.toolleveling.debug.CommandLevelTool;
     name = "TinkerToolLeveling",
     dependencies = "required-after:forge@[14.21.1.2410,);"
                    + "required-after:mantle@[1.12-1.3.1.21,);"
-                   + "required-after:tconstruct@[1.12-2.8,)",
+                   + "required-after:tconstruct@[1.12-2.10,)",
     acceptedMinecraftVersions = "[1.12,1.13)"
 )
 public class TinkerToolLeveling {
